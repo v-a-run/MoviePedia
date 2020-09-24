2796dd50b9aa2db4952e7bcfa80874212f3eb44f
Varun Kumar <vkvarun26@gmail.com>
2020-09-25 01:25:24 +0530

    change http to https

--- public/js/MovieWars.js
+++ public/js/MovieWars.js
@@ -48,7 +48,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get("http://www.omdbapi.com/",{
+    const response = await axios.get("https://www.omdbapi.com/",{
         params : {
             apikey : '89078b54',
             i : movie.imdbID
